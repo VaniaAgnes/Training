f4d438270f0e417e5d65cb06fd729e2ff2b639b6
split bill calculator vania <vania.djunaedy@binus.ac.id>
2022-09-29 18:44:59 +0700

    exercise 5&6

---
+++ exercise 5.py
@@ -0,0 +1,8 @@
+vx= int(input("Value of vx:"))
+vy= int(input("Value of vy:"))
+wx= int(input("Value of wx:"))
+wy= int(input("Value of wy:"))
+
+result= (2*wx)-vx, (2*wy)-vy
+print(result)
+
---
+++ exercise 6).py
@@ -0,0 +1,10 @@
+
+
+
+Burger_Selling_Price = float(input("Price of the Burger:$"))
+Soda_Selling_Price = float(input("Price of the Soda:$"))
+Combo_Meal_Selling_Price = float(input("Price of the Combo Meal:$"))
+
+profit = (Burger_Selling_Price+Soda_Selling_Price)-(Combo_Meal_Selling_Price)
+print(profit)
+
